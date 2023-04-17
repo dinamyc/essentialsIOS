ff8dbc875662dcce764d0f4ebf796c426a44667b
Jose Luis Enriquez <j.luis.enriquez77@gmail.com>
2023-04-17 11:18:45 +0200

    First commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jose Luis Enriquez on 17/04/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL:URL
+}
---
+++ EssentialFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Jose Luis Enriquez on 17/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
