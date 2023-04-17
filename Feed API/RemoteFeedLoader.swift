86e8a5d117a94361319d5ed527f8355984bc309a
Jose Luis Enriquez <j.luis.enriquez77@gmail.com>
2023-04-17 14:17:59 +0200

    move remotefeedLoader components to production

---
+++ Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeedTests
+//
+//  Created by Jose Luis Enriquez on 17/04/2023.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
