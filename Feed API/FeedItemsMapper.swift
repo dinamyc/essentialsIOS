4368714d4dd9257b212b503a24e57c5ac51dfe3a
Jose Luis Enriquez <j.luis.enriquez77@gmail.com>
2023-04-27 22:45:50 +0200

    Guarantee we do not deliver a result (invoke the completion closure) after the "RemoteFeedLoader" instance has been deallocated

--- Feed API/FeedItemsMapper.swift
+++ Feed API/FeedItemsMapper.swift
@@ -29,27 +29,11 @@ internal final class FeedItemsMapper {
     
     private static var OK_200: Int { return 200 }
     
-//    internal static func map(_ data: Data,_ response: HTTPURLResponse) throws -> [FeedItem]{
-//        guard response.statusCode == OK_200 else {
-//            throw RemoteFeedLoader.Error.invalidData
-//        }
-//
-//        let root = try JSONDecoder().decode(Root.self, from: data)
-//        return root.items.map({ $0.item })
-//    }
-    
     internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
         guard response.statusCode == OK_200,
               let root = try? JSONDecoder().decode(Root.self, from: data)else {
             return .failure(.invalidData)
         }
-        //        do {
-        //          let root = try JSONDecoder().decode(Root.self, from: data)
-        //let items = root.items.map({ $0.item })
-        //response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
         return .success(root.feed)
-        //        } catch {
-        //            return .failure(.invalidData)
-        //        }
     }
 }
